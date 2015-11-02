a1b16ff2f509ff237fa9f2a89860da8ef7022b20
Jonas Coch <jonas@iosphere.de>
2015-11-02 19:19:49 +0100

    Removes other implementation

--- src/Simplify/RadialDistance.elm
+++ src/Simplify/RadialDistance.elm
@@ -24,38 +24,19 @@ run tolerance points =
       [] -> points
       [x] -> points
       [x,y] -> points
-      --_ -> run' (abs tolerance) points
-      _ -> run'' (abs tolerance) [] points
+      _ -> run' (abs tolerance) [] points
 
+-- TODO: Don't forget to add the last point when needed
 
-run' : Float -> List Point -> List Point
-run' tolerance points =
-  case points of
-    [] -> []
-    [x] -> points
-    [x,y] -> points
-    x::xs ->
-      let
-        isClose point = distance x point < tolerance
-        tail = run' tolerance <| dropWhile isClose xs
-        simplified =
-          case tail of
-            -- Make sure that the last element in the simplified list is always
-            -- the last element from the original list.
-            [] -> (List.reverse >> List.take 1) xs
-            _ -> x :: tail
-      in
-        simplified
-
-run'' : Float -> List Point -> List Point -> List Point
-run'' tolerance accum points =
+run' : Float -> List Point -> List Point -> List Point
+run' tolerance accum points =
   case points of
     [] -> accum
     x::xs ->
       let
         isClose point = distance x point < tolerance
       in
-        run''
+        run'
           tolerance
           (List.append accum [x])
           (dropWhile isClose xs)
